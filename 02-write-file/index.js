1bbc22e7584c4f995bbe4d9fa3f98100eee6b2e3
AvKa3003 <kirillka3003@gmail.com>
2021-11-08 21:01:31 +0300

    feat: add ex 1 and 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+var stream =  fs.createWriteStream(path.join(__dirname, 'text.txt'));
+console.log("Введите строку");
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+process.openStdin().on("keypress", function(chunk, key) {
+    if(key && key.name === "c" && key.ctrl) {
+      console.log("bye bye");
+      process.exit();
+    }
+});
+
+function writeInFile() {
+    rl.question('', (answer) => {
+        if (answer == "exit"){
+            rl.close();
+            console.log("Конец ввода");
+        } else {
+            stream.write(answer + '\n');
+            writeInFile();
+        }
+    });
+}
+
+writeInFile();
