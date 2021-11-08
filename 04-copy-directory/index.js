7480eea9b7fc0661e72d80f05c1819192cd2df63
AvKa3003 <kirillka3003@gmail.com>
2021-11-08 23:34:08 +0300

    feat: add ex 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    fs.readdir(path.join(__dirname, "files"), {withFileTypes: true}, (err, files) => {
+        files.forEach(file => {
+            fs.createReadStream(path.join(__dirname, "files", file.name)).pipe(fs.createWriteStream(path.join(__dirname, "files-copy", file.name)));
+        });
+    });
+})
